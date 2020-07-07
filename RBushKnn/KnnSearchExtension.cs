21bec78de828c2a2c23cd29d90b98c7045f6a098
Кирилл Захаров <kirill78zah@gmail.com>
2020-07-07 19:42:58 +0300

    knn search

---
+++ RBushKnn/KnnSearchExtension.cs
@@ -0,0 +1,62 @@
+﻿using RBush;
+using System;
+using System.Collections.Generic;
+
+namespace RBushKnn
+{
+	public static class KnnSearchExtension
+	{
+		public static IReadOnlyList<T> KnnSearch<T>(this RBush<T> tree, double x, double y, int n,
+			Func<T, bool> predicate = null, double maxDist = -1) where T : ISpatialData
+		{
+			if (maxDist > 0)
+				maxDist = maxDist * maxDist;//compare quadratic distances
+
+			List<T> result = new List<T>();
+
+			//priority queue
+			C5.IntervalHeap<SpatialDataWrapper> queue
+				= new C5.IntervalHeap<SpatialDataWrapper>(new DistComparer());
+
+			RBush<T>.Node node = tree.Root;
+
+			while (node != null)
+			{
+				foreach (ISpatialData child in node.Children)//for each child
+				{
+					SpatialDataWrapper childDistData = new SpatialDataWrapper(child, x, y);//calc distance to box
+					if (maxDist < 0 || childDistData.SquaredDistanceToBox <= maxDist)//check if distance less than max distance
+						queue.Add(childDistData);//add to queue
+				}
+
+				//dequeue all objects that are items stored in RBush
+				while (queue.Count > 0 && queue.FindMin().SpatialData is T)
+				{
+					SpatialDataWrapper candidateWr = queue.DeleteMin();//this item goes to result
+					T candidate = (T)candidateWr.SpatialData;
+					if (predicate == null || predicate.Invoke(candidate))//if element satisfy the condition
+						result.Add(candidate);//add to result
+					if (n > 0 && result.Count == n)//if the desired amount is already in the result
+						return result;//return result
+				}
+
+				//process next element in queue
+				if (queue.Count > 0)
+					node = queue.DeleteMin().SpatialData as RBush<T>.Node;
+				else
+					node = null;
+			}
+
+			return result;
+		}
+
+		private class DistComparer : IComparer<SpatialDataWrapper>
+		{
+			public int Compare(SpatialDataWrapper n1, SpatialDataWrapper n2)
+			{
+				return n1.SquaredDistanceToBox.CompareTo(n2.SquaredDistanceToBox);
+			}
+
+		}
+	}
+}
